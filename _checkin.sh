1774f08fa30ec95d6445a8b236268d2175b53772
Joseph Hunt <jodyhunt@hotmail.com>
2024-08-05 11:20:26 -0500

    test

--- _checkin.sh
+++ _checkin.sh
@@ -3,10 +3,13 @@ if [[ "$1" == "" ]]; then
   echo "Usage: $0 <commit-comment>"
   exit -1
 fi
+pushd bot-server; rm -rf logs; popd
+pushd bot-ui; rm -rf logs; popd
 echo "Disk usage:"
 du -sh .
 gitleaks detect -r .gitleaks
-#read -p "Do you wish to continue? " yn
+cat .gitleaks
+read -p "Do you wish to continue? " yn
 yn='y'
 case $yn in
   [Yy]* )
