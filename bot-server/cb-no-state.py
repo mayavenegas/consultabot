91108e1c7ccfe8d69a27932461eb5439b6b91a12
Joseph Hunt <jodyhunt@hotmail.com>
2024-08-07 09:10:33 -0500

    blue: fix empty response

--- bot-server/cb-no-state.py
+++ bot-server/cb-no-state.py
@@ -12,6 +12,8 @@ from langchain_community.llms import LlamaCpp
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.callbacks import BaseCallbackHandler, CallbackManager, StreamingStdOutCallbackHandler
+from langchain_core.messages import BaseMessage
+from langchain_core.outputs import LLMResult
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
