26b4c61b6e7dfd7a015136a9e9756306a51114f3
Joseph Hunt <jodyhunt@hotmail.com>
2024-08-07 10:28:40 -0500

    blue: fix empty response

--- bot-server/cb-no-state.py
+++ bot-server/cb-no-state.py
@@ -148,49 +148,49 @@ class emptyCallbackHandler(BaseCallbackHandler):
     def on_llm_start(
         self, serialized: Dict[str, Any], prompts: List[str], **kwargs: Any
     ) -> Any:
-        """Run when LLM starts running."""
+        print(f"LLM {serialized.get('name')} started")
 
     def on_chat_model_start(
         self, serialized: Dict[str, Any], messages: List[List[BaseMessage]], **kwargs: Any
     ) -> Any:
-        """Run when Chat Model starts running."""
+        print(f"Chat {serialized.get('name')} started")
 
     def on_llm_new_token(self, token: str, **kwargs: Any) -> Any:
         """Run on new LLM token. Only available when streaming is enabled."""
 
     def on_llm_end(self, response: LLMResult, **kwargs: Any) -> Any:
-        """Run when LLM ends running."""
+        print("LLM ended.")
 
     def on_llm_error(
         self, error: Union[Exception, KeyboardInterrupt], **kwargs: Any
     ) -> Any:
-        """Run when LLM errors."""
+        print(f"LLM error: {error}")
 
     def on_chain_start(
         self, serialized: Dict[str, Any], inputs: Dict[str, Any], **kwargs: Any
     ) -> Any:
-        """Run when chain starts running."""
+        print(f"Chain {serialized.get('name')} started")
 
     def on_chain_end(self, outputs: Dict[str, Any], **kwargs: Any) -> Any:
-        """Run when chain ends running."""
+        print(f"Chain ended, outputs: {outputs}")
 
     def on_chain_error(
         self, error: Union[Exception, KeyboardInterrupt], **kwargs: Any
     ) -> Any:
-        """Run when chain errors."""
+        print(f"Chain error: {error}")
 
     def on_tool_start(
         self, serialized: Dict[str, Any], input_str: str, **kwargs: Any
     ) -> Any:
-        """Run when tool starts running."""
+        print(f"Tool {serialized.get('name')} started")
 
     def on_tool_end(self, output: Any, **kwargs: Any) -> Any:
-        """Run when tool ends running."""
+        print(f"Tool ended, outputs: {outputs}")
 
     def on_tool_error(
         self, error: Union[Exception, KeyboardInterrupt], **kwargs: Any
     ) -> Any:
-        """Run when tool errors."""
+        print(f"Tool error: {error}")
 
     def on_text(self, text: str, **kwargs: Any) -> Any:
         """Run on arbitrary text."""
