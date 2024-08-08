c64fc6a486bd56a2e400b89e7e5ff3727e050913
Joseph Hunt <jodyhunt@hotmail.com>
2024-08-08 13:29:39 -0500

    green: add CVE scanning

--- setup/0-ubuntu-update.sh
+++ setup/0-ubuntu-update.sh
@@ -6,4 +6,4 @@ sudo apt -y install net-tools git-lfs
 # install grype CVE scanner
 curl -sSfL https://raw.githubusercontent.com/anchore/grype/main/install.sh | sudo sh -s -- -b /usr/local/bin
 # install syft SBOM generator
-curl -sSfL https://raw.githubusercontent.com/anchore/syft/main/install.sh | sh -s -- -b /usr/local/bin
+curl -sSfL https://raw.githubusercontent.com/anchore/syft/main/install.sh | sudo sh -s -- -b /usr/local/bin
