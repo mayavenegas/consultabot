3f45d8ef6a43d7811434dead038aba99cdb795f1
Joseph Hunt <jodyhunt@hotmail.com>
2024-08-09 10:00:52 -0500

    green: GPU layers = 43

--- bot-server/start-botserver.sh
+++ bot-server/start-botserver.sh
@@ -2,4 +2,5 @@
 mkdir -p ./logs
 tmux new -s bot-server uvicorn cb-no-state:app	\
         --host 0.0.0.0 --port 9000      	\
+        --workers 2                             \
         --log-level info
